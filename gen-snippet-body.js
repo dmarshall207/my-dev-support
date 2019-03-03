8a43fc090cee42f58d1512fdd3ac917841090aae
dlmarshall207 <dlmarshall207@gmail.com>
2019-03-03 12:42:32 -0600

    3/3/19:12:42p

--- gen-snippet-body.js
+++ gen-snippet-body.js
@@ -1,10 +1,16 @@
-// @:context: eplr gen of snippet 'body'
+// @:context: tool to generate snippet 'body' from file_path
+
 /*
 https://stackoverflow.com/questions/2447915/javascript-string-regex-backreferences
 http://webreference.com/js/column5/values.html
 */
 
+// ===========================================================
+// require
 let fs            = require('fs')
+
+// ========================================================
+// core
 function backslash_dbl_quotes(s) {    // good
     // return str 's' with added '\' to double quotes chars
     return s.replace( /([""])/g, '\\$1')}
@@ -18,4 +24,19 @@ function gen_snippet_body(input_path) {
     let dat_as_snippet_body = `body: [\n${sig.join('\n')}\n]`
     // fs.writeFileSync('./out.js', dat_as_snippet_body)
     console.log(dat_as_snippet_body)}
-gen_snippet_body('./input.js')
+// gen_snippet_body('./input.js')
+
+// ==========================================================
+// main 
+console.log( process.argv )
+if (process.argv.length < 3) {
+    console.log('\nERR : command-line : invalid format')
+    console.log('   * explects: #1 arg:      input file\n')
+    process.exit(1)}
+let input_file_path         = process.argv[2];
+console.log(`input_file_path: ${input_file_path}`)
+if (! fs.existsSync(input_file_path)){
+    console.log('\nERR: input-file')
+    console.log(`  * input-file not found: ${input_file_path}\n`)
+    process.exit(1)}
+gen_snippet_body(input_file_path)
