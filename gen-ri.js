67fa9636b843349f4705acead18cfde0efeb4340
dlmarshall207 <dlmarshall207@gmail.com>
2019-03-15 10:53:02 -0500

    3/15/19 added gen-ri

---
+++ gen-ri.js
@@ -0,0 +1,2 @@
+let gen_ri              = require('./tools-01').gen_ri
+console.log(gen_ri())
--- tools-01.js
+++ tools-01.js
@@ -1,4 +1,5 @@
 let fs                  = require('fs')
+
 function copy_file(
     file_name_to_copy,
     src_dir_path,
@@ -23,4 +24,22 @@ function copy_file(
             process.exit(1)}
         fs.copyFileSync(src_file_path, dst_file_path)}
 
+function gen_ri() {
+    let getRandomInt = (min, max)=>{
+        /* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random */
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;} //The maximum is exclusive and the minimum is inclusive
+    let i           = 4;  // number of digits
+    let sig         = []
+    while (i > 0) {
+        let n       = getRandomInt(0, 16).toString(16)
+        if (getRandomInt(0, 2) === 0 ){   // randomize case
+            sig.push(n.toUpperCase())} 
+        else {
+            sig.push(n)}
+        i   = i-1}
+    return `${sig.join('')}`}
+
 exports.copy_file                   = copy_file
+exports.gen_ri                      = gen_ri
