7a747b703320e73852e3c8c10f3fa721b8a2a4b2
dlmarshall207 <dlmarshall207@gmail.com>
2019-03-21 16:06:05 -0500

    3/21

--- install-snippets-file.js
+++ install-snippets-file.js
@@ -1,5 +1,5 @@
 let {copy_file}     = require('./tools-01')
 copy_file(
     'dm1.code-snippets',
-    process.env.SnippetsDirPath,
-    `${process.env.StoRoot}\\my-doc`)
+    `${process.env.StoRoot}\\my-doc`,
+    process.env.SnippetsDirPath)
