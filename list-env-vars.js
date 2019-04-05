eb5ccdbb8a2ac1db1652c26bd3390f328aa3178f
dlmarshall207 <dlmarshall207@gmail.com>
2019-04-05 06:57:39 -0500

    4/5

--- list-env-vars.js
+++ list-env-vars.js
@@ -1,11 +1,19 @@
 /* 
-idea:  list all evn vars
+list all evn vars
 */
 
+const fs       = require('fs')
+let r          = process.env
+for (var k in process.env) {
+    if (process.env.hasOwnProperty(k)) {
+        let v       = process.env[k];
+        console.log(`${k}`.padStart(30), `|`, v)
+    }}
 
-const {spawn,
-    spawnSync}      = require("child_process")
-const fs            = require("fs");
-const powershell    = "powershell.exe"
-const o             = spawnSync(powershell, ["gci env:*", '|', 'sort-object', 'name']);
-console.log(`${o.stdout}`)
+
+// const {spawn,
+//        spawnSync}   = require("child_process")
+// const fs            = require("fs");
+// const powershell    = "powershell.exe"
+// const r             = spawnSync(powershell, ["gci env:*", '|', 'sort-object', 'name']);
+// console.log(`${r.stdout}`)
